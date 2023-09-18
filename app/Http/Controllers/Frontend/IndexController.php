3f029526a56953da8f2cbff75444e11ae9a270f2
congdeptrai13 <cong102.201@gmail.com>
2023-09-18 20:22:54 +0700

    finish setup property details page

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Amenities;
+use App\Models\Facility;
+use App\Models\MultiImage;
+use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -11,6 +16,15 @@ class IndexController extends Controller
 
     public function PropertyDetails($id, $slug)
     {
-        return view("frontend.property.property_details");
+        $property = Property::find($id);
+        $admin = User::where("role", "admin")->where("username", "admin")->first();
+        $multiImage = MultiImage::where("property_id", $id)->get();
+        $amenities = $property->amenities_id;
+        $property_amen = explode(",", $amenities);
+        $facilities = Facility::where("property_id", $id)->get();
+        $type_id = $property->ptype_id;
+        $relatedProperty = Property::where("ptype_id", $type_id)->where("id", "!=", $id)->orderBy("id", "DESC")->limit(3)->get();
+
+        return view("frontend.property.property_details", compact("property", 'admin', 'multiImage', 'property_amen', 'facilities', 'relatedProperty'));
     }
 }
